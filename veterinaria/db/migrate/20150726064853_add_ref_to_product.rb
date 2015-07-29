78eed427d88ff9ed4c5dc87e8d592d15cffc472f
graziella <graziellavillafuerte@gmail.com>
2015-07-29 05:07:14 +0000

    cambios migraciones

--- veterinaria/db/migrate/20150726064853_add_ref_to_product.rb
+++
@@ -1,5 +0,0 @@
-class AddRefToProduct < ActiveRecord::Migration
-  def change
-    add_reference :products, :unit_measure, index: true, foreign_key: true
-  end
-end
--- veterinaria/db/migrate/20150728005428_remove_patient_from_citations.rb
+++
@@ -1,7 +0,0 @@
-class RemovePatientFromCitations < ActiveRecord::Migration
-  def change
-    remove_reference :citations, :patient_id, index: true, foreign_key: true
-    #remove_foreign_key :citations, :patient
-    remove_column :citations, :patient_id
-  end
-end
