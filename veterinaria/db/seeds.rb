256ebb9c9310b630389a5144b8d2c300cddf7ae9
graziella <graziellavillafuerte@gmail.com>
2015-07-30 05:09:43 +0000

    Formato

--- veterinaria/db/seeds.rb
+++ veterinaria/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+   departments = Department.create([{ name: 'Amazonas' }, { name: 'Ancash' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
